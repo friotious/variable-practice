44daf5097206787f8249761f5d9241094c528c6c
Rio Foster <friotious@gmail.com>
2021-10-19 16:56:49 -0700

    Add booean variables

--- mode1-1.js
+++ mode1-1.js
@@ -1,9 +1,17 @@
 // variables that use strings
+//
 var make = "Toyota"
 var model = "Tundra"
 var edition = "sr5"
+//
 // variables that use numbers
 //
 var doors = 4
 var miles = 239147
 var weight = 4518
+//
+// variables that use booleans
+//
+var hasFullSizeBed = false
+var hasTowHitch = true
+var has4wd = true
