137712ee3032a40108e93943c4bf859698439ea9
Rio Foster <friotious@gmail.com>
2021-10-19 17:44:37 -0700

    Add object example

--- mode1-1.js
+++ mode1-1.js
@@ -22,3 +22,11 @@ var extras = ['bed liner', 'stereo', 'fog lights']
 var dingsAndDents = ['tailgate', 'front passenger side', 'rear driver side']
 var recentWork = ['front struts', 'front brakes', 'front left bearings']
 //
+//variables that use objects
+//
+var myTruck = {
+  make: 'Toyota',
+  miles: 239147,
+  has4wd: true,
+  extras: ['front struts', 'front brakes', 'front left bearings'],}
+  
